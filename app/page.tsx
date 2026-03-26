37d803ce626ce5dc284d0ad4370e45cba4b96bd0
sayeed <expsayeedshorif@gmail.com>
2026-03-26 20:45:48 +0600

    working on orderflow components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Feature from "@/components/FeatureSection/Feature";
 import Home from "@/components/HomePage/home";
 import Marquee from "@/components/MarqueeLine";
 import News from "@/components/newsSection/news";
+import OrderFlow from "@/components/OrderFlow/OrderFlow";
 import Products from "@/components/Products/Products";
 
 
@@ -14,6 +15,7 @@ export default function page() {
         <Marquee/>
         <Products/>
         <Marquee/>
+        <OrderFlow/>
     </div>
   );
 }
---
+++ components/OrderFlow/OrderFlow.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const OrderFlow = () => {
+    return (
+        <div className='py-[5%]'>
+            {/* title section  */}
+            <div className='text-center'>
+                <h1 className="font-outfit  text-[#FFF455] text-4xl sm:text-6xl md:text-7xl font-black  uppercase 
+                [-webkit-text-stroke:2px_black]  [paint-order:stroke_fill]">
+                    how  to  order
+                </h1>
+                <p className='sm:text-3xl text-xl py-2 font-semibold'>Order flow</p>
+            </div>
+        </div>
+    );
+};
+
+export default OrderFlow;
